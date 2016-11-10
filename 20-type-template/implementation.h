df30e0c0391a09fddf8986a5af442f4cad02c7ee
MazurMilosz <milosz.mazur92@gmail.com>
2016-11-10 11:53:19 +0100

    Milosz Mazur template specialization for boost::optional <>

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,30 +1,63 @@
 #pragma once
 
-template<typename T>
+#include <boost/optional.hpp>
+
+template<typename T>                                                        //szablon podstawowy
 class container_wrapper
 {
 public:
 
-    container_wrapper() = default;              //konstruktor dzieki default jest generowany domyslnie
-    
-   // container_wrapper( T arg ) : _value( arg )                              // robimy kopie obiektu typu T -> robimy za duzo nie powinnismy kopiowac typu zamieniamy na move
+    container_wrapper() = default;                                          //konstruktor dzieki default jest generowany domyslnie
+
+   // container_wrapper( T arg ) : _value( arg )                            //robimy kopie obiektu typu T -> robimy za duzo nie powinnismy kopiowac typu zamieniamy na move
     container_wrapper( T arg ): _value( std::move( arg ) )
     {
     }
-    
+
     container_wrapper( const container_wrapper &) = default;                //konstruktor kopiujacy
 
     container_wrapper( container_wrapper &&) = default;                     //move constructor
     container_wrapper & operator=( const container_wrapper &) = default;    //przypisuje do wrappera ten operator przypisania
-    container_wrapper & operator=( container_wrapper &&) = default;         //przypisanie rvalue operatora   
-    
+    container_wrapper & operator=( container_wrapper &&) = default;         //przypisanie rvalue operatora
 
-    std::size_t size() const 
+
+    std::size_t size() const
     {
         return _value.size();
     }
 private:
-    T _value; //rozpoczecie od _ tylko dla prywatnych pewna konwencja i uwazac jak sie uzywa
+    T _value;                                                              //rozpoczecie od _ tylko dla prywatnych pewna konwencja i uwazac jak sie uzywa
+
+};
+
+
+
+
+template<typename T>                                                                  //specjalizacja szablonu
+class container_wrapper<boost::optional<T>>
+{
+public:
+
+    container_wrapper() = default;                                          //konstruktor dzieki default jest generowany domyslnie
+
+   // container_wrapper( T arg ) : _value( arg )                            //robimy kopie obiektu typu T -> robimy za duzo nie powinnismy kopiowac typu zamieniamy na move
+    container_wrapper( boost::optional<T> arg ): _value( std::move( arg ) )
+    {
+    }
+
+    container_wrapper( const container_wrapper &) = default;                //konstruktor kopiujacy
+
+    container_wrapper( container_wrapper &&) = default;                     //move constructor
+    container_wrapper & operator=( const container_wrapper &) = default;    //przypisuje do wrappera ten operator przypisania
+    container_wrapper & operator=( container_wrapper &&) = default;         //przypisanie rvalue operatora
+
+
+    std::size_t size() const
+    {
+        return _value ? 1 : 0;
+    }
+private:
+    boost::optional<T> _value;                                                              //rozpoczecie od _ tylko dla prywatnych pewna konwencja i uwazac jak sie uzywa
 
 };
 
